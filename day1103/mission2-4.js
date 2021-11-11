7dff7ac7ea96f0675fc80ff4bb39de3b9b54d1a1
songyunho98 <song502122120@gmail.com>
2021-11-11 14:08:35 +0900

    배열 만들기

--- day1103/mission2-4.js
+++ day1103/mission2-4.js
@@ -1,11 +1,4 @@
-/*
-4. 배열 만들기
-아래 데이터를 확인한다.
-https://gist.github.com/crongro/ade2c3f74417fc202c8097214c965f27
-숫자타입으로만 구성된 요소를 뽑아 배열만들기
+//4번 배열 만들기
 
-//실행결과
-["width", "height", "hOffset", "vOffset", "size", "hOffset", "vOffset"]
-*/
 
 const o = require('./o.js');
