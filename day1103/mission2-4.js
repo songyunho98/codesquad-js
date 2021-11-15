458f96486a96ae7fa8e9bd6e7811879c3e215df8
songyunho98 <song502122120@gmail.com>
2021-11-15 11:27:17 +0900

    배열 만들기

--- day1103/mission2-4.js
+++ day1103/mission2-4.js
@@ -1,4 +1,13 @@
 //4번 배열 만들기
 
-
 const o = require('./o.js');
+
+let array = [];
+
+for(let object in o.data) {
+    for(let key in o.data[object]) {
+        if (typeof o.data[object][key] === 'number')
+            array.push(key);
+    }       
+}
+console.log(array);
