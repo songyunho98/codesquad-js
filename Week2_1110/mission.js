1f62625652062cd86fa5d4b358537dea2873b71a
songyunho98 <song502122120@gmail.com>
2021-11-17 10:40:45 +0900

    괄호문법 검사기

---
+++ Week2_1110/mission.js
@@ -0,0 +1,13 @@
+const data1 = "[1,2,[3,4,[5,[6]]]]";
+const data2 = "[1,2,[3,4,[5,[6]]"
+
+function ObjectInformation(data) {
+    for(let i = 0; i < data.length; i++) {
+        
+    }
+}
+
+run(data1);
+run(data2);
+
+  
