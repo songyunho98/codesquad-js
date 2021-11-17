dcd16ea7d109c82ce7715dd79f2cfe5bda877e5d
songyunho98 <song502122120@gmail.com>
2021-11-17 10:46:29 +0900

    배열 거르기

---
+++ Week1_1103/mission2-2.js
@@ -0,0 +1,22 @@
+//2번 배열 거르기
+const peoples = ["crong!@#", "honux5", "sarah#", "hea3d", "zello", "5lucas"];
+function filterId(array) {
+    const regExp = /[\{\}\[\]\/?.,;:|\)*~`!^\-+<>@\#$%&\\\=\(\'\"]/;
+    
+    // 특수 기호 포함 요소 제거
+    for (let i = 0; i < array.length; i++) {
+        if (regExp.test(array[i])) {
+            array.splice(i, 1);
+            i--;
+        }
+    }
+
+    // 아이디에서 숫자를 제거
+    for (let i = 0; i < array.length; i++) 
+        array[i] = array[i].replace(/[0-9]/g, "");
+
+    console.log(array);
+    
+    return array;
+}
+filterId(peoples)
